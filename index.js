ac9a69bb547d829b4bcba721680db35edbc69a97
saisilinus <saisilinus19@gmail.com>
2020-05-05 06:07:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const Realm = require('realm');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});

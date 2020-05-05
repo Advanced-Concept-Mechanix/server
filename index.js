b4a69f420cd578cb41f84007ed5539d23ff2cd8e
saisilinus <saisilinus19@gmail.com>
2020-05-05 10:50:59 -0400

    installed blockchain dependencies

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.use(bodyParser.json());
 const MONGODB = `mongodb://127.0.0.1:27017/shanga`;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || MONGODB, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || MONGODB, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("db connected...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

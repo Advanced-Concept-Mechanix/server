d30d50a51f0dc677b0979ba724aeea21d27e056c
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-3.local>
2020-05-07 02:47:33 +0300

    staging tests for the app

---
+++ test/unit.js
@@ -0,0 +1,6 @@
+//with unit,  we test inidividual code blocks
+//we use the describe block to define an occurrence of the test.
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const package = require ('package.json');

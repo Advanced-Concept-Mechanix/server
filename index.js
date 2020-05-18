c7704798e165f7a76975ee35f410cfba1d0d4f46
saisilinus <saisilinus19@gmail.com>
2020-05-18 09:28:13 -0400

    next()

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ process.on('unhandledRejection', err => {
 
 process.on('uncaughtException', err => {
   console.log(err.name, err.message);
+  console.log(err);
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
 
   process.exit(1);

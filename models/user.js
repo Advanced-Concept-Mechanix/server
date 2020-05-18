1bf56279a45e8b1164b3e7fc4798087a89538ad8
saisilinus <saisilinus19@gmail.com>
2020-05-18 12:58:36 -0400

    email validation added

--- models/user.js
+++ models/user.js
@@ -5,17 +5,15 @@ const pubKey = require('../blockchain/pubKey');
 //const SecretKey = require('../blockchain/secretKey');
 const secretKey = require('../blockchain/secretKey');
 
-// var validateEmail = function(email) {
-//     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     return re.test(email)
-// };
-
-// validate: [validateEmail, "Please enter a valid email address"]
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
 let userSchema = new mongoose.Schema({
     name: {type:String, unique: true},
     password: String,
-    email: {type:String, unique: true},
+    email: {type:String, unique: true, validate: [validateEmail, "Please enter a valid email address"]},
     phone: Number,
     company: String,
     type: {type:String, enum: ["manufacturer", "distributor", "retailer", "end-user"]},

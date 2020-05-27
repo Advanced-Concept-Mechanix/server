03171cfd06b792511e6826f394c512420002eab8
saisilinus <saisilinus19@gmail.com>
2020-05-27 06:32:47 -0400

    query for transaction changed

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -105,7 +105,7 @@ router.get('/validity/:id', async function(req, res, next){
         }
     });
 
-    let query = {'txSummary._id': req.params.id};
+    let query = {txSummary:{$elemMatch:{_id:req.params.id}}};
     console.log(query);
 
     Block.find(query, function(err, tx){

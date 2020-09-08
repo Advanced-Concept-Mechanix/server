861b74e161247b74d0f4e93d26df3631dbcc4718
saisilinus <saisilinus19@gmail.com>
2020-09-08 09:41:18 -0400

    fixed compare + cleaned up

--- imageProcessing/imageHash.js
+++ imageProcessing/imageHash.js
@@ -1,37 +1,18 @@
 const { imageHash }= require('image-hash');
+const imghash = require('imghash');
 const compare = require('hamming-distance');
 
-// remote file simple
-// imageHash('https://ichef-1.bbci.co.uk/news/660/cpsprodpb/7F76/production/_95703623_mediaitem95703620.jpg', 16, true, (error, data) => {
-//   if (error) throw error;
-//   console.log(data);
-//   // 0773063f063f36070e070a070f378e7f1f000fff0fff020103f00ffb0f810ff0
-// });
+const hash1 = imghash.hash('./images/north_korea.jpg');
+const hash2 = imghash.hash('./images/north_korea.jpg');
 
-const hashImg = (url) => {
-    let pHash = imageHash(url, 64, true, (error, data) => {
-        if(error){
-            console.log(`Error: ${error}`)
-            throw error;
-        }
-        // let hashStr = bin2String(pHash);
-        // let hashStr = data.toString();
-        // console.log(`hash: ${data}, length:${data.length}, type:${data.type}`);
-        return data.toString();
-    })
-    return pHash;
-}
-
-let hashEx = hashImg('./images/north_korea.jpg');
-console.log(`hash Example: ${hashEx}`);
-// let strEx = convertToString(hashEx);
-// console.log(`string example: ${strEx}`);
-
-// hashImg('./images/north_korea_1.jpg');
-// let phash1 = hashImg('./images/north_korea.jpg');
-// let phash2 = hashImg('./images/north_korea_2.jpg')
-// let distance = compare(phash1, phash2);
-// console.log(phash1, phash2)
-// console.log(bin2String(phash1));
-// var distance = compare(Buffer.from(phash1, 'hex'), Buffer.from(phash2, 'hex'));
-// console.log(`distance: ${distance}`);
+Promise
+  .all([hash1, hash2])
+  .then((results) => {
+    const dist = compare(results[0], results[1]);
+    console.log(`Distance between images is: ${dist}`);
+    if (dist <= 12) {
+      console.log('Images are similar');
+    } else {
+      console.log('Images are NOT similar');
+    }
+});
